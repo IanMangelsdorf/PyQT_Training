68b38e976c24130ed05e06b3bd89d1f6b6aca6d3
IanMangelsdorf <mangelsdorfian@gmail.com>
2019-12-14 12:19:43 +1000

    Initial Load of Mastering GUI Programing with Python Chapter 2 a

--- Chapter2.py
+++ Chapter2.py
@@ -9,9 +9,62 @@ class MainWindow(qtw.QWidget):
         #Main Window Consctuctor
         super().__init__()
         #Main Code Goes Here
+        subwidgit = qtw.QWidget(self, toolTip= 'This is a Tool Tip Widget String')
+
+        lable = qtw.QLabel('Hello Widgets!', self)
+        lable.setText('Hi There Widgets')
+        print (lable.text())
+
+        line_edit = qtw.QLineEdit('Default Value', self)
+
+        line_edit = qtw.QLineEdit(
+            'default value',
+            self,
+            placeholderText = 'Type Here',
+            clearButtonEndable= True,
+            maxLength = 20
+        )
+
+        button = qtw.QPushButton('Push Me')
+        button = qtw.QPushButton(
+            'Push Me',
+            self,
+            checkable = True,
+            checked = True,
+            shortcut = qtg.QKeySequence('Ctrl+p')
+        )
+
+        combobox = qtw.QComboBox(self,
+                                 editable = True,
+                                 insertPolicy= qtw.QComboBox.InsertAtTop)
+        combobox.addItem('Lemon',1)
+        combobox.addItem('Peach','Oah I like Peaches',
+        combobox.addItem('Strawberry', qtw.QWidget),
+        combobox.insertItem(1,'Radish',2)
+        )
+
+        spinbox = qtw.QSpinBox(
+            self,
+            value =12,
+            maximum =100,
+            minimum = 10,
+            prefix = '$',
+            suffix = ' + Tax',
+            singleStep = 5
+        )
+
+        datetimebox = qtw.QDateTimeEdit(
+            self,
+            date = qtc.QDate.currentDate(),
+            time = qtc.QTime(12,30),
+            calenderPopus = True,
+            maximumDate = qtc.QDate(2030,1,1),
+            maximumTime = qtc.QTime(17,0),
+            dsplayFormat = 'yyy-MM-dd HH:mm'
+        )
+
 
         #End Main Code
-
         self.show()
 
 
--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,8 @@
-from PyQt5 import QtWidgets
-
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import QtCore as qtc
 app = QtWidgets.QApplication([])
 
-window = QtWidgets.QWidget(windowTitle='Hello QT')
+window = QtWidgets.QWidget( windowTitle='Hello QT')
+#window.setWindowFlag(qtc.Qt.Sheet|qtc.Qt.Popup)
 window.show()
 app.exec()
