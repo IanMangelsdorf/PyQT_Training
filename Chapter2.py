dc6da2421bb05a85879498e2c72c9a30093eb0a8
IanMangelsdorf <mangelsdorfian@gmail.com>
2019-12-14 12:45:00 +1000

    Initial Load of Mastering GUI Programing with Python Chapter 2 b

--- Chapter2.py
+++ Chapter2.py
@@ -15,17 +15,17 @@ class MainWindow(qtw.QWidget):
         lable.setText('Hi There Widgets')
         print (lable.text())
 
-        line_edit = qtw.QLineEdit('Default Value', self)
+        #line_edit = qtw.QLineEdit('Default Value', self)
 
         line_edit = qtw.QLineEdit(
             'default value',
             self,
             placeholderText = 'Type Here',
-            clearButtonEndable= True,
+            clearButtonEnabled= True,
             maxLength = 20
         )
 
-        button = qtw.QPushButton('Push Me')
+        #utton = qtw.QPushButton('Push Me')
         button = qtw.QPushButton(
             'Push Me',
             self,
@@ -37,11 +37,12 @@ class MainWindow(qtw.QWidget):
         combobox = qtw.QComboBox(self,
                                  editable = True,
                                  insertPolicy= qtw.QComboBox.InsertAtTop)
+
         combobox.addItem('Lemon',1)
-        combobox.addItem('Peach','Oah I like Peaches',
-        combobox.addItem('Strawberry', qtw.QWidget),
+        combobox.addItem('Peach','Oah I like Peaches')
+        combobox.addItem('Strawberry', qtw.QWidget)
         combobox.insertItem(1,'Radish',2)
-        )
+
 
         spinbox = qtw.QSpinBox(
             self,
@@ -57,17 +58,52 @@ class MainWindow(qtw.QWidget):
             self,
             date = qtc.QDate.currentDate(),
             time = qtc.QTime(12,30),
-            calenderPopus = True,
+            calendarPopup = True,
             maximumDate = qtc.QDate(2030,1,1),
             maximumTime = qtc.QTime(17,0),
-            dsplayFormat = 'yyy-MM-dd HH:mm'
+            displayFormat = 'yyyy-MM-dd HH:mm'
         )
 
 
         #End Main Code
         self.show()
 
+        textedit= qtw.QTextEdit(
+            self,
+            acceptRichText = False,
+            lineWrapMode= qtw.QTextEdit.FixedColumnWidth,
+            lineWrapColumnOrWidth= 25,
+            placeholderText = 'Enter Your Stuff Here'
+        )
 
+        layout = qtw.QVBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(lable)
+        layout.addWidget(line_edit)
+
+        subLayout = qtw.QHBoxLayout()
+        layout.addLayout(subLayout)
+
+        subLayout.addWidget(button)
+        subLayout.addWidget(combobox)
+
+        gridLayout = qtw.QGridLayout()
+        layout.addLayout(gridLayout)
+        gridLayout.addWidget(spinbox,0,0)
+        gridLayout.addWidget(datetimebox,0,1)
+        gridLayout.addWidget(textedit,1,0,2,2)
+
+        form_layout = qtw.QFormLayout()
+        layout.addLayout(form_layout)
+
+        form_layout.addRow('Item 1', qtw.QLineEdit(self))
+        form_layout.addRow('Item 2', qtw.QLineEdit(self))
+        form_layout.addRow((qtw.QLabel('<b>This is a LAbel only Row </b>')))
+
+        lable.setFixedSize(150, 40)
+        line_edit.setMinimumSize(150,15)
+        line_edit.setMaximumSize(500,15)
 if __name__== '__main__':
     app = qtw.QApplication(sys.argv)
     mw = MainWindow()
